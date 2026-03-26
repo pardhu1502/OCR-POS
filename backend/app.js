4458bc46417edd0efc660dc9572c66a10adfd196
pardhu <pardhu1502@gmail.com>
2026-03-27 00:24:01 +0530

    serve frontend instead of api message

--- backend/app.js
+++ backend/app.js
@@ -3,27 +3,25 @@ import cors from "cors";
 import processRoutes from "./routes/processRoutes.js";
 import path from "path";
 
-
 const app = express();
 
 app.use(cors());
 app.use(express.json());
-app.get("/", (req, res) => {
-  res.send("API is running...");
-});
+
+
 app.use("/api", processRoutes);
 
 const __dirname = path.resolve();
 
-// Serve frontend static files
+//  Serve frontend static files
 app.use(express.static(path.join(__dirname, "../frontend/dist")));
 
-// Catch-all fallback (NO route pattern)
+//  Catch-all (serves React app)
 app.use((req, res) => {
   res.sendFile(path.join(__dirname, "../frontend/dist/index.html"));
 });
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
-  console.log(`Server running on port http://localhost:${PORT}`);
+  console.log(`Server running on port ${PORT}`);
 });
