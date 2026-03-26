d3a99270720c023f5f14b4069993586c1757ab96
pardhu <pardhu1502@gmail.com>
2026-03-27 00:09:17 +0530

    fix wildcard route for render

--- backend/app.js
+++ backend/app.js
@@ -19,7 +19,7 @@ const __dirname = path.resolve();
 app.use(express.static(path.join(__dirname, "../frontend/dist")));
 
 // Handle React routing
-app.get("*", (req, res) => {
+app.get("/*", (req, res) => {
   res.sendFile(path.join(__dirname, "../frontend/dist/index.html"));
 });
 
