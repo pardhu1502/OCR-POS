1f5acbf9a2ddd35e6841f02525714c4dfb112aea
pardhu <pardhu1502@gmail.com>
2026-03-27 09:20:37 +0530

    fix backend

--- backend/middleware/upload.js
+++ backend/middleware/upload.js
@@ -1,12 +1,7 @@
 import multer from "multer";
 
-const storage = multer.diskStorage({
-  destination: function (req, file, cb) {
-    cb(null, "uploads/");
-  },
-  filename: function (req, file, cb) {
-    cb(null, Date.now() + "-" + file.originalname);
-  },
-});
+const storage = multer.memoryStorage();
 
-export const upload = multer({ storage });
+export const upload = multer({
+  storage,
+});
