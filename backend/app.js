f39b87a63dd8b62d5e39e93e85466d28bc7cb88b
pardhu <pardhu1502@gmail.com>
2026-03-27 09:29:08 +0530

    previous checkpoint

--- backend/app.js
+++ backend/app.js
@@ -8,6 +8,10 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
+app.use((req, res, next) => {
+  console.log("Incoming:", req.method, req.url);
+  next();
+});
 
 
 app.use("/api", processRoutes);
